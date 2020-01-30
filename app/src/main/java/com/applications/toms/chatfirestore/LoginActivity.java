cf7c1b1c74b6035cc58edd70481bdcccddb2f76f
Toms <tomas.manuel.vazquez@gmail.com>
2020-01-30 16:54:45 -0300

    v 0.2 Authentication Firebase email&pass

---
+++ app/src/main/java/com/applications/toms/chatfirestore/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.applications.toms.chatfirestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
